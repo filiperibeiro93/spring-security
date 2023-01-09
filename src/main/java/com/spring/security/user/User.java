466c43b44e9452be6978d490df000b04aa1ccbae
Filipe Ribeiro <facribeiro.93@gmail.com>
2023-01-09 11:46:53 -0300

    properties and creating user

---
+++ src/main/java/com/spring/security/user/User.java
@@ -0,0 +1,25 @@
+package com.spring.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
