4ae6c8d5b779ca3360c99adf0ad17c2606985ca8
Filipe Ribeiro <facribeiro.93@gmail.com>
2023-01-09 14:26:20 -0300

    starting security and adding dependencies

--- src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
@@ -15,11 +15,21 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
     }
 }
