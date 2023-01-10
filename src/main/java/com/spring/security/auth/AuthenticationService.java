eb90bbd06f2e5f1ba95b474b30096f518b427dc3
Filipe Ribeiro <facribeiro.93@gmail.com>
2023-01-10 10:20:03 -0300

    endpoints and register user

---
+++ src/main/java/com/spring/security/auth/AuthenticationService.java
@@ -0,0 +1,38 @@
+package com.spring.security.auth;
+
+import com.spring.security.config.JwtService;
+import com.spring.security.user.Role;
+import com.spring.security.user.User;
+import com.spring.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder encoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(encoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
